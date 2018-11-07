c96f7dc4a9c5cb04a904fe14fecd29d4ce4ce8b2
Ryan <ryannitz@amazon.com>
2018-11-07 18:31:09 -0500

    Initial

---
+++ test.py
@@ -0,0 +1,17 @@
+import boto3, os, sys, logging, glob
+
+def main():
+
+    FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=FORMAT)
+    logger = logging.getLogger(__name__)
+
+    logger.info('Hello task')
+
+    # Run other commands
+    logger.info(glob.glob('/*'))
+
+    logger.info('Done')
+
+if __name__ == '__main__':
+    main()
